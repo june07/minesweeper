3bab00e359880908a3d53714122e2e979dcf765b
will <667@onezerohosting.com>
2019-07-27 19:01:46 -0700

    Added flags.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 cd vue
 #perl -pi -e 's/TRAVIS_BUILD_NUMBER=(.*)/TRAVIS_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER/' .env
-echo "VERSION=$(grep "\"version\"" vue/package.json | cut -f2 -d":" | cut -f2 -d"\"")" >> .env
+echo "VERSION=$(grep "\"version\"" package.json | cut -f2 -d":" | cut -f2 -d"\"")" >> .env
 cat .env
 npm install
 npm run build
