bbd0a22aa3ab33d815cc17721e7249be46d4b17e
will <667@onezerohosting.com>
2019-07-27 17:43:50 -0700

    .

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 cd vue
 #perl -pi -e 's/TRAVIS_BUILD_NUMBER=(.*)/TRAVIS_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER/' .env
 echo "TRAVIS_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER" >> .env
+cp .env dist/.env
+cat .env
 npm install
 npm run build
 
