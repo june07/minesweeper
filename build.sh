f01a941b2a59bd1a0ef3e78c181163f57158c407
will <667@onezerohosting.com>
2019-07-27 17:41:17 -0700

    .

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 cd vue
 #perl -pi -e 's/TRAVIS_BUILD_NUMBER=(.*)/TRAVIS_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER/' .env
 echo "TRAVIS_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER" >> .env
